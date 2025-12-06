b52f6c9e69047e4fce5b948f69757a51b61ddd70
JPA_Jack <jack.parsons@jpasolutions.co.uk>
2025-12-06 19:13:43 +0000

    pygame update

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 pygame.init()
+clock = pygame.time.Clock()
+dt = 0
 
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,7 +20,10 @@ def main():
                 return
         log_state()
         screen.fill ("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        player.update(dt)
 
 if __name__ == "__main__":
     main()
