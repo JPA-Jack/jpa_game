032d42e116f44e20d05810f24152e501af0f6910
JPA_Jack <jack.parsons@jpasolutions.co.uk>
2025-12-12 18:43:53 +0000

    Pygame Update

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 import sys
 
 pygame.init()
@@ -16,11 +17,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     asteroidfield = AsteroidField()
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
---
+++ shot.py
@@ -0,0 +1,13 @@
+import constants as ct
+from circleshape import CircleShape
+import pygame
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white",self.position, ct.SHOT_RADIUS, ct.LINE_WIDTH)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
