76c312024d44c9145e9abcb847d9d519bf2251a6
JPA_Jack <jack.parsons@jpasolutions.co.uk>
2025-12-12 21:00:16 +0000

    PYGAME UPDATE

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import constants as ct
 from circleshape import CircleShape
 import pygame
+import random
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ct.ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            new_angle = random.uniform(20, 50)
+            angle1 = self.velocity.rotate(new_angle)
+            angle2 = self.velocity.rotate(-new_angle)
+            new_radius = self.radius - ct.ASTEROID_MIN_RADIUS
+            asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+            asteroid1.velocity = angle1 * 1.2
+            asteroid2.velocity = angle2 * 1.2
+
+
+            
